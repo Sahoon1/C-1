17e8f51f19d0a4e206afadb3c775a0391ab3d3b6
Sahoon Hussain Ansari <162339117+Sahoon1@users.noreply.github.com>
2024-10-11 20:28:09 +0500

    work

---
+++ Percentage . App/Percentage . App/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Percentage.App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTotalMarks = new System.Windows.Forms.TextBox();
+            this.textBoxObtainedMarks = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Percentage App";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tot Mark";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Obt Mark";
+            // 
+            // textBoxTotalMarks
+            // 
+            this.textBoxTotalMarks.Location = new System.Drawing.Point(341, 137);
+            this.textBoxTotalMarks.Name = "textBoxTotalMarks";
+            this.textBoxTotalMarks.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTotalMarks.TabIndex = 2;
+            // 
+            // textBoxObtainedMarks
+            // 
+            this.textBoxObtainedMarks.Location = new System.Drawing.Point(341, 194);
+            this.textBoxObtainedMarks.Name = "textBoxObtainedMarks";
+            this.textBoxObtainedMarks.Size = new System.Drawing.Size(100, 20);
+            this.textBoxObtainedMarks.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Result Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AccessibleName = "labelResult";
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(496, 140);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(117, 18);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "Your Result is";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxObtainedMarks);
+            this.Controls.Add(this.textBoxTotalMarks);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTotalMarks;
+        private System.Windows.Forms.TextBox textBoxObtainedMarks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Percentage . App/Percentage . App/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Percentage.App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Get total marks from the user input
+            if (!double.TryParse(textBoxTotalMarks.Text, out double totalMarks) || totalMarks <= 0)
+            {
+                MessageBox.Show("Please enter a valid positive number for total marks.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Get obtained marks from the user input
+            if (!double.TryParse(textBoxObtainedMarks.Text, out double obtainedMarks) || obtainedMarks < 0 || obtainedMarks > totalMarks)
+            {
+                MessageBox.Show($"Please enter a valid number of marks obtained (0 - {totalMarks}).", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Calculate percentage
+            double percentage = (obtainedMarks / totalMarks) * 100;
+
+            // Determine grade
+            string grade;
+            if (percentage >= 90)
+            {
+                grade = "A";
+            }
+            else if (percentage >= 80)
+            {
+                grade = "B";
+            }
+            else if (percentage >= 70)
+            {
+                grade = "C";
+            }
+            else if (percentage >= 60)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "F";
+            }
+
+            // Display the result
+            labelResult.Text = $"You scored: {obtainedMarks}/{totalMarks}\nPercentage: {percentage:F2}%\nGrade: {grade}";
+        }
+    }
+    }
+
