1d9fe32939bfbc65c3576694be5e8e0e111ef202
Sahoon Hussain Ansari <162339117+Sahoon1@users.noreply.github.com>
2024-10-30 20:44:58 +0500

    work

---
+++ DaTa BaSe/DaTa BaSe/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DaTa_BaSe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-QI7HNRF;Initial Catalog=crudapp;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into users values(@id,@name,@age)", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data Saved");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-QI7HNRF;Initial Catalog=crudapp;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update users set name=@name, age=@age where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data Updated");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-QI7HNRF;Initial Catalog=crudapp;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete users where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data Deleted");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-QI7HNRF;Initial Catalog=crudapp;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from users", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
