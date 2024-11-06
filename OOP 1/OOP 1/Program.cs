624031be4b9aed7fee1a08c6123d36a358da08ea
Sahoon Hussain Ansari <162339117+Sahoon1@users.noreply.github.com>
2024-11-06 20:29:42 +0500

    work

---
+++ OOP 1/OOP 1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    class Program
+    {
+        public abstract class Vehicle
+        {
+          
+            public abstract void StartEngine();
+            public abstract void StopEngine();
+        }
+
+        public class Car : Vehicle
+        {
+            public override void StartEngine()
+            {
+                Console.WriteLine("Car engine started");
+            }
+
+            public override void StopEngine()
+            {
+                Console.WriteLine("Car engine stopped");
+            }
+        }
+
+        public class Motorcycle : Vehicle
+        {
+            public override void StartEngine()
+            {
+                Console.WriteLine("Motorcycle engine started");
+            }
+
+            public override void StopEngine()
+            {
+                Console.WriteLine("Motorcycle engine stopped");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Vehicle myCar = new Car();
+            myCar.StartEngine();
+            myCar.StopEngine();
+
+            Vehicle myBike = new Motorcycle();
+            myBike.StartEngine();
+            myBike.StopEngine();
+            Console.ReadKey();
+        }
+    }
+}
